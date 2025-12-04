a1c5beb4632b44b3d4f6204e4bd887634ed5413d
Vishwash-chauchan <vishwashchauhan77@gmail.com>
2025-12-04 17:06:40 +0530

    changes user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,7 +5,22 @@ const userSchema = new mongoose.Schema(
     clerkId: { type: String, required: true, unique: true },
     email: { type: String },
     name: { type: String },
-    imageUrl: { type: String }
+    imageUrl: { type: String },
+    plan: {
+      type: String,
+      enum: ["No Plan", "Silver", "Gold", "Platinum"],
+      default: "No Plan"
+    },
+    noOfSearches: {
+      type: Number,
+      default: 0
+    },
+    type: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user"
+    }
+    
   },
   { timestamps: true }
 );
